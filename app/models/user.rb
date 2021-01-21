c1edd77f6f70247b5bb9244b3b1588159b69c68d
Ramrin <iwgmw.nir02@gmail.com>
2021-01-21 17:39:10 +0900

    バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # 空の場合はDBに保存しない
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
+
 end
