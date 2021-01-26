7dc103ef3aee7024b9c528d32361d079a509d82b
Ramrin <iwgmw.nir02@gmail.com>
2021-01-26 16:36:46 +0900

    shipping_cost_idの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,8 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
 
 
   # 空の場合はDBに保存しない
@@ -18,6 +20,8 @@ class Item < ApplicationRecord
   # 選択が「---」の時は保存できないようにする
   validates :category_id, numericality: { other_than: 1 }
   validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_cost_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
 
 
 end
