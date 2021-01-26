5b1e98de9347c802bc004a90a36a9293723b007b
Ramrin <iwgmw.nir02@gmail.com>
2021-01-26 18:30:06 +0900

    ActiveHashの各データ整理

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -50,7 +50,7 @@ class Prefecture < ActiveHash::Base
     {id: 48, name: '沖縄県'}, 
    ]
 
-  # 1つの発送元の地域はたくさんの商品に紐づいている
-  include ActiveHash::Associations
-  has_many :items
-end
+    # 1つの発送元の地域はたくさんの商品に紐づいている
+    include ActiveHash::Associations
+    has_many :items
+  end
--- app/models/shipping_day.rb
+++ app/models/shipping_day.rb
@@ -1,12 +1,12 @@
 class ShippingDay <  ActiveHash::Base
   self.data = [
-    {id: 1, name: '---'}
-    {id: 2, name: '1~2日で発送'}
-    {id: 3, name: '2~3日で発送'}
+    {id: 1, name: '---'},
+    {id: 2, name: '1~2日で発送'},
+    {id: 3, name: '2~3日で発送'},
     {id: 4, name: '4~7日で発送'}
   ]
 
-  # 1つの配送料の負担はたくさんの商品に紐づいている
-  include ActiveHash::Associations
-  has_many :items
-end
+    # 1つの配送料の負担はたくさんの商品に紐づいている
+    include ActiveHash::Associations
+    has_many :items
+  end
