b935ad32f1940aa7fcb7be49c05dd91bdc2066ff
Ramrin <iwgmw.nir02@gmail.com>
2021-01-26 15:28:11 +0900

    ActiveStorageの導入 ②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,10 @@ class ItemsController < ApplicationController
   def index
   end
 
+  private
+
+  # imageという名前で送られてきた画像ファイルの保存を許可する
+  def item_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  # item.imageで、添付されたファイルにアクセスできるようになる
+  has_one_attached :image
 end
