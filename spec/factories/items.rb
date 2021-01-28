f59b4478cf1e4ce7ffa7e4b3de65ec04d3f6a107
Ramrin <iwgmw.nir02@gmail.com>
2021-01-28 22:29:59 +0900

    itemモデルの単体テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
-    
+    name             {Faker::Name.name}
+    introduction     {Faker::Lorem.sentence}
+    price            {"55555"}
+    category_id      {2}
+    condition_id      {2}
+    shipping_cost_id {2}
+    prefecture_id    {2}
+    shipping_day_id  {2}
+    association :user
+
+    # 生成するダミーデータに画像を添付する
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,92 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品出品機能' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+    it '全ての情報が存在すれば保存できること' do
+      expect(@item).to be_valid
+    end
+  
+    it 'nameが空だと保存できないこと' do
+      @item.name = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'introductionが空だと保存できないこと' do
+      @item.introduction = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Introduction can't be blank")
+    end
+
+    it 'priceが空だと保存できないこと' do
+      @item.price = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'imageが空だと保存できないこと' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+
+
+    it 'category_idが1だと保存できないこと' do
+      @item.category_id = "1"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
+    end
+
+    it 'condition_idが1だと保存できないこと' do
+      @item.condition_id = "1"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Condition must be other than 1")
+    end
+
+    it 'shipping_cost_idが1だと保存できないこと' do
+      @item.shipping_cost_id = "1"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping cost must be other than 1")
+    end
+
+    it 'prefecture_idが1だと保存できないこと' do
+      @item.prefecture_id = "1"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+    end
+
+    it 'shipping_day_idが1だと保存できないこと' do
+      @item.shipping_day_id = "1"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping day must be other than 1")
+    end
+
+    it 'priceが¥300より小さいと保存できないこと' do
+      @item.price = "100"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+    end
+
+    it 'priceが¥9,999,999より大きいと保存できないこと' do
+      @item.price = "10000000"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+    end
+
+    it 'priceは半角数字でないと保存できないこと' do
+      @item.price = "１０００"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not a number")
+    end
+
+    it 'userが紐づいていないと保存できないこと' do
+      @item.user = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("User must exist")
+    end
+  end
 end
