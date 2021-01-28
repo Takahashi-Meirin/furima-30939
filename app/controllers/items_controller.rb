b0ba79ad59031714ceeea7185015ec9182b43ec1
Ramrin <iwgmw.nir02@gmail.com>
2021-01-28 21:02:50 +0900

    商品の情報をDBに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,7 +21,7 @@ class ItemsController < ApplicationController
 
   private
 
-  # imageという名前で送られてきた画像ファイルの保存を許可する
+  # 出品する情報の保存を許可する
   def item_params
     params.require(:item).permit(:image, :name, :introduction, :price, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, :user_id).merge(user_id: current_user.id)
   end
