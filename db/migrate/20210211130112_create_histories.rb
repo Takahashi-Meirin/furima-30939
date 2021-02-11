2c79e86cfa22e03fc7f1117d42930e7890efad24
Ramrin <iwgmw.nir02@gmail.com>
2021-02-11 22:09:40 +0900

    購入履歴のモデルの作成

---
+++ db/migrate/20210211130112_create_histories.rb
@@ -0,0 +1,12 @@
+class CreateHistories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :histories do |t|
+      # user_idカラムを外部キー制約でつける
+      t.integer :history_id,    null: false, foreign_key: true
+      # item_idカラムを外部キー制約でつける
+      t.integer :history_id,    null: false, foreign_key: true
+      
+      t.timestamps
+    end
+  end
+end
