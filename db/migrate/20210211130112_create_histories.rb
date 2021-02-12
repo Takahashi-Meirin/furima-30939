93e9308127fce5c2a40e21326fc4e70644397a56
Ramrin <iwgmw.nir02@gmail.com>
2021-02-12 20:15:36 +0900

    マイグレーションファイル　修正

--- db/migrate/20210211130112_create_histories.rb
+++ db/migrate/20210211130112_create_histories.rb
@@ -2,9 +2,9 @@ class CreateHistories < ActiveRecord::Migration[6.0]
   def change
     create_table :histories do |t|
       # user_idカラムを外部キー制約でつける
-      t.integer :history_id,    null: false, foreign_key: true
+      t.integer :user_id,    null: false, foreign_key: true
       # item_idカラムを外部キー制約でつける
-      t.integer :history_id,    null: false, foreign_key: true
+      t.integer :item_id,    null: false, foreign_key: true
       
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_061058) do
+ActiveRecord::Schema.define(version: 2021_02_11_130112) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,25 @@ ActiveRecord::Schema.define(version: 2021_01_26_061058) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "destinations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "post_code", null: false
+    t.string "municipality", null: false
+    t.string "address", null: false
+    t.string "building"
+    t.string "phone_number", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "history_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "histories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "introduction", null: false
@@ -46,6 +65,12 @@ ActiveRecord::Schema.define(version: 2021_01_26_061058) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
